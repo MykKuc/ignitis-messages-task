b2240a8f6cb764cc3d186d9a1e63187a76bb4715
Mykolas <mykolas.kucinskas2@gmail.com>
2023-01-21 13:34:52 +0200

    initialized the project

---
+++ src/main/java/com/mykolas/ignitismessagetask/IgnitisMessageTaskApplication.java
@@ -0,0 +1,13 @@
+package com.mykolas.ignitismessagetask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgnitisMessageTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgnitisMessageTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mykolas/ignitismessagetask/IgnitisMessageTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mykolas.ignitismessagetask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IgnitisMessageTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
