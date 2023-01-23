9257d8e85263a69e651906bf74c9a1bde37f7ad3
Mykolas <mykolas.kucinskas2@gmail.com>
2023-01-23 21:17:52 +0200

    added total messages, fist and last messages dates and more Liquibase mock data

--- src/main/java/com/mykolas/ignitismessagetask/message/MessageRepository.java
+++ src/main/java/com/mykolas/ignitismessagetask/message/MessageRepository.java
@@ -1,10 +1,20 @@
 package com.mykolas.ignitismessagetask.message;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface MessageRepository extends JpaRepository<Message,Long> {
 
     List<Message> findAll();
+
+    Integer countAllByAuthorId(Long id);
+
+    @Query(value = "SELECT MIN(time) FROM messages WHERE author_id = ?1", nativeQuery = true)
+    LocalDateTime findByEarliestMeesageTime(Long userId);
+
+    @Query(value = "SELECT MAX(time) FROM messages WHERE author_id = ?1", nativeQuery = true)
+    LocalDateTime findLastMessageDate(Long userId);
 }
--- src/main/java/com/mykolas/ignitismessagetask/statistics/StatisticsService.java
+++ src/main/java/com/mykolas/ignitismessagetask/statistics/StatisticsService.java
@@ -30,8 +30,15 @@ public class StatisticsService {
         for(User user:listOfAllUsers){
 
             Statistic userStatistic = new Statistic();
+
             userStatistic.setUserId(user.getId());
             userStatistic.setUserEmail(user.getEmail());
+
+            userStatistic.setTotalMessages(messageRepository.countAllByAuthorId(user.getId()));
+
+            userStatistic.setFirstMessage(messageRepository.findByEarliestMeesageTime(user.getId()));
+
+            userStatistic.setLastMessage(messageRepository.findLastMessageDate(user.getId()));
 
             allUserStatisticsArray.add(userStatistic);
         }
